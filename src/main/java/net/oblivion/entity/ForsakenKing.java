0fee4e23e31cfa1fc7ea9d34f52e00629ef992d8
Globox1997 <Micha.Damaschke@web.de>
2026-02-06 15:40:27 +0100

    playing around with world gen

---
+++ src/main/java/net/oblivion/entity/ForsakenKing.java
@@ -0,0 +1,14 @@
+package net.oblivion.entity;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.world.World;
+
+public class ForsakenKing extends HostileEntity {
+
+    public ForsakenKing(EntityType<? extends HostileEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+}
